570ac63b8bd5403158766d4fe52c5bd23bb923dc
Massimiliano Fadda <72360779+massi-fa@users.noreply.github.com>
2021-02-06 00:36:43 +0100

    LinkTree
    finito Link Tree

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from "./components/Home";
+import styled from "styled-components/macro";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const Container = styled.div`
+	height: 100%; 
+`
+
+const App = () => (
+  <Container>
+    <Home/>
+  </Container>
+);
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,65 @@
+import React from "react";
+import styled from "styled-components/macro";
+
+import spotify from "../res/spotify5.svg";
+import youtube from "../res/youtube.svg";
+import instagram from "../res/instagram.svg";
+import sfondo from "../res/sfondo.jpg";
+
+const Container = styled.div`
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	background: url(${(props)=>props.imgUrl});
+	background-repeat: no-repeat;
+	background-size: cover;  
+`
+const Button1 = styled.a`
+	outline: none;
+  border: 0;
+	margin: auto auto 25px auto;
+	background-color: transparent;
+`;
+const Button2 = styled.a`
+  outline: none;
+  border: 0;
+	margin: 25px auto 25px auto;
+	background-color: transparent;
+`;
+const Button3 = styled.a`
+  outline: none;
+  border: 0;
+	margin: 25px auto auto auto;
+	background-color: transparent;
+`;
+const Img = styled.img`
+	height: 20vh;
+	filter: drop-shadow(0 0 0.75rem #aabc);
+	&:hover{
+		transform: scale(1.5);
+	};
+`;
+
+const H1 = styled.h1`
+	color: white;
+  font-size: 25px;
+  margin: 0px 10px 5px auto;
+	font-family: 'Sriracha', cursive;
+`;
+
+const Home = () => (
+	<Container imgUrl={sfondo}>
+		<Button1 href='https://www.instagram.com/tv/CK7MC3Fsrys/?utm_source=ig_web_copy_link'>
+			<Img src={youtube} alt="Error"/>	
+		</Button1>
+		<Button2 href='https://open.spotify.com/artist/4jpSaw00mW1r415vcQUDNQ?si=LZT9dc_cRcaGwYu5hFHF5Q'>
+			<Img src={spotify} alt="Error"/>	
+		</Button2>
+		<Button3 href='https://www.instagram.com/rickypino/'>
+			<Img src={instagram} alt="Error"/>
+		</Button3>
+		<H1>Ricky Pino</H1>
+	</Container>
+);
+
+export default Home;
